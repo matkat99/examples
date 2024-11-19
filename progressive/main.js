c81bf960f9dc509227a1ca56071b4beda4251ab3
Shane T <st83440@gmail.com>
2024-11-19 14:43:43 -0700

    changes

--- progressive/main.js
+++ progressive/main.js
@@ -1,27 +1,31 @@
-document
-  .getElementById("contact-form")
-  .addEventListener("submit", function (event) {
-    var name = document.getElementById("name").value;
-    var email = document.getElementById("email").value;
-    var error = "";
+function submitForm(event) {
+  const nameInput = document.querySelector("#name");
+  const emailInput = document.querySelector("#email");
+  // or could do this:
+  // const nameInput = this.name
+  // const emailInput = this.email;
+  console.log(this.name.value);
+  let error = "";
 
-    if (name === "") {
-      error += "Name is required.\n";
-    }
+  if (nameInput.value === "") {
+    error += "Name is required.\n";
+  }
 
-    if (email === "") {
-      error += "Email is required.\n";
-    } else if (!validateEmail(email)) {
-      error += "Please enter a valid email address.\n";
-    }
+  if (emailInput.value === "") {
+    error += "Email is required.\n";
+  } else if (!validateEmail(email)) {
+    error += "Please enter a valid email address.\n";
+  }
 
-    if (error) {
-      event.preventDefault();
-      document.getElementById("form-error").textContent = error;
-    }
-  });
+  if (error) {
+    event.preventDefault();
+    document.getElementById("form-error").textContent = error;
+  }
+}
 
 function validateEmail(email) {
   var re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   return re.test(email);
 }
+
+document.getElementById("contact-form").addEventListener("submit", submitForm);
